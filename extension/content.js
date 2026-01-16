003c0aa4720aab22d0db5f04d0361a848ebd8b38
Seweryn Krakowiak <254786@edu.p.lodz.pl>
2026-01-16 22:59:46 +0100

    fix: error when searching

--- extension/content.js
+++ extension/content.js
@@ -1,6 +1,6 @@
 // Konfiguracja API
 const CONFIG = {
-  BASE_URL: "https://ad.tsuki.com.pl/api/downloader", // Zmieniono na ścieżkę Next.js API
+  BASE_URL: "http://100.67.98.49:3002/api/downloader", // Zmieniono na ścieżkę Next.js API
   BUTTON_TEXT: "Szukaj Torrenta (Nyaa)",
   BUTTON_CLASS_HIANIME: "btn btn-sm btn-primary ml-2",
   BUTTON_CLASS_ANILIST: "nyaa-anilist-btn", // Custom class for AniList
